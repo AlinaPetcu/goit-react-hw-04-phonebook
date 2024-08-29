4ec2bda052739e925629d60a553acf25134b9907
AlinaPetcu <150749536+AlinaPetcu@users.noreply.github.com>
2024-08-29 03:23:38 +0300

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,67 +3,52 @@ import ContactForm from './Contact/Contact';
 import ContactList from './ContactList/ContactList';
 import FilterContacts from './FilterContact/FilterContacts';
 
-class App extends Component {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    search: '',
+
+export const App = () => {
+  const [contacts, setContacts] = useState([
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ]);
+  const [filter, setFilter] = useState('');
+
+  const deleteContact = contactId => {
+    setContacts(contacts.filter(contact => contact.id !== contactId));
   };
 
-  addContact = newContact => {
-    const { contacts } = this.state;
-
-    // Check if the contact already exists
+  const addContact = newContact => {
     const contactExists = contacts.some(
       contact => contact.name === newContact.name
     );
 
     if (!contactExists) {
-      this.setState(prevState => ({
-        contacts: [...prevState.contacts, newContact],
-      }));
+      setContacts([...contacts, newContact]);
     } else {
       alert('The contact is already in the phonebook');
     }
   };
 
-  deleteContact = id => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== id),
-    }));
+  const handleFilterChange = filter => {
+    setFilter(filter);
   };
 
-  handleSearchChange = value => {
-    this.setState({ search: value });
+  const getFilteredContacts = () => {
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
   };
 
-  render() {
-    const { contacts, search } = this.state;
+  return (
+    <div>
+      <ContactForm addContact={addContact} />
+      <FilterContacts filter={filter} onFilterChange={handleFilterChange} />
+      <Contacts
+        contacts={getFilteredContacts()}
+        deleteContact={deleteContact}
+      />
+    </div>
+  );
+};
 
-    // Filter contacts based on search input
-    const filteredContacts = contacts.filter(contact =>
-      contact.name.toLowerCase().includes(search.toLowerCase())
-    );
-
-    return (
-      <div>
-        <ContactForm addContact={this.addContact} />
-
-        <FilterContacts
-          value={this.state.search}
-          onChange={this.handleSearchChange}
-        />
-        <ContactList
-          contacts={filteredContacts}
-          deleteContact={this.deleteContact}
-        />
-      </div>
-    );
-  }
-}
-
-export { App };
+export default App;
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -2,44 +2,37 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import css from './ContactList.module.css';
 
-class ContactList extends Component {
-  constructor(props) {
-    super(props);
-    this.handleDelete = this.handleDelete.bind(this);
-  }
+const Contacts = ({ contacts, deleteContact }) => {
+  return (
+    <div className={css.listContainer}>
+      <h2 className={css.contactName}>Contacts</h2>
+      <ul>
+        {contacts.map(contact => (
+          <li className={css.contactItem} key={contact.id}>
+            {contact.name} : {contact.number}
+            <button
+              className={styles.deleteButton}
+              type="button"
+              onClick={() => deleteContact(contact.id)}
+            >
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
-  handleDelete(id) {
-    this.props.deleteContact(id);
-  }
-
-  render() {
-    const { contacts } = this.props;
-
-    return (
-      <div className={css.listContainer}>
-        <h2 className={css.contactName}>Contacts</h2>
-        <ul>
-          {contacts.map(contact => (
-            <li className={css.contactItem} key={contact.id}>
-              {contact.name} : {contact.number}
-              <button
-                className={css.deleteButton}
-                type="button"
-                onClick={() => this.handleDelete(contact.id)}
-              >
-                Delete
-              </button>
-            </li>
-          ))}
-        </ul>
-      </div>
-    );
-  }
-}
-
-ContactList.propTypes = {
-  contacts: PropTypes.array.isRequired,
+Contacts.propTypes = {
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    })
+  ).isRequired,
   deleteContact: PropTypes.func.isRequired,
 };
 
-export default ContactList;
+export default Contacts;
